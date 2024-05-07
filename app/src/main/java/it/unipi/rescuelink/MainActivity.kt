923421dd8a146b2beccec7b1e334dfff938a37fc
Ettore Ricci <minecraft.etto@gmail.com>
2024-05-07 18:16:14 +0200

    Map view activity

--- app/src/main/java/it/unipi/rescuelink/MainActivity.kt
+++ app/src/main/java/it/unipi/rescuelink/MainActivity.kt
@@ -1,6 +1,11 @@
 package it.unipi.rescuelink
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +21,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {v -> this.changeToMapsView(v)}
+    }
+
+    private fun changeToMapsView(v: View?)
+    {
+        Log.d(null, "Changing to map view")
+        startActivity(Intent(this, MapsActivity::class.java))
     }
 }
