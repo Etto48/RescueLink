18ae2b9d6c3ae0ab9edbd29b67135649b4378e67
Ettore Ricci <minecraft.etto@gmail.com>
2024-05-25 19:41:06 +0200

    Updated GATT callbacks and added automatic read from other clients, needs testing on multiple real devices
    Signed-off-by: Ettore Ricci <minecraft.etto@gmail.com>

--- app/src/main/java/it/unipi/rescuelink/adhocnet/AdHocNetWorker.kt
+++ app/src/main/java/it/unipi/rescuelink/adhocnet/AdHocNetWorker.kt
@@ -6,13 +6,14 @@ import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothGatt
 import android.bluetooth.BluetoothGattCallback
 import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothGattServer
 import android.bluetooth.BluetoothGattServerCallback
+import android.bluetooth.BluetoothGattService
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.content.Context
 import android.util.Log
-import android.widget.Toast
 import androidx.annotation.RequiresPermission
 import androidx.work.Worker
 import androidx.work.WorkerParameters
@@ -23,13 +24,25 @@ import java.util.UUID
 class AdHocNetWorker(appContext: Context, workerParameters: WorkerParameters) :
     Worker(appContext, workerParameters) {
 
-    val infoCharacteristic = BluetoothGattCharacteristic(
+    private val infoCharacteristic = BluetoothGattCharacteristic(
         UUID.fromString("9578d7f6-3fe4-404c-b2cc-841747b6163f"),
-        BluetoothGattCharacteristic.PROPERTY_READ or BluetoothGattCharacteristic.PROPERTY_WRITE,
-        BluetoothGattCharacteristic.PERMISSION_READ or BluetoothGattCharacteristic.PERMISSION_WRITE
+        BluetoothGattCharacteristic.PROPERTY_READ,
+        BluetoothGattCharacteristic.PERMISSION_READ
+    )
+    private val infoService = BluetoothGattService(
+        UUID.fromString("9578d7f6-3fe4-404c-b2cc-841747b6163f"),
+        BluetoothGattService.SERVICE_TYPE_PRIMARY
     )
 
+    init {
+        infoService.addCharacteristic(infoCharacteristic)
+    }
+
+    private var bluetoothGattServer: BluetoothGattServer? = null
+
     private var context = appContext
+
+    private var connectedGattSet = mutableMapOf<String,BluetoothGatt>()
 
     private var gattServerCallback = object : BluetoothGattServerCallback() {
         override fun onConnectionStateChange(device: BluetoothDevice?, status: Int, newState: Int) {
@@ -44,30 +57,17 @@ class AdHocNetWorker(appContext: Context, workerParameters: WorkerParameters) :
         ) {
             super.onCharacteristicReadRequest(device, requestId, offset, characteristic)
             if (characteristic != null && characteristic.uuid == infoCharacteristic.uuid) {
-                // TODO: handle read request
-            }
-        }
-
-        override fun onCharacteristicWriteRequest(
-            device: BluetoothDevice?,
-            requestId: Int,
-            characteristic: BluetoothGattCharacteristic?,
-            preparedWrite: Boolean,
-            responseNeeded: Boolean,
-            offset: Int,
-            value: ByteArray?
-        ) {
-            super.onCharacteristicWriteRequest(
-                device,
-                requestId,
-                characteristic,
-                preparedWrite,
-                responseNeeded,
-                offset,
-                value
-            )
-            if (characteristic != null && characteristic.uuid == infoCharacteristic.uuid) {
-                // TODO: handle write request
+                try {
+                    bluetoothGattServer?.sendResponse(
+                        device,
+                        requestId,
+                        BluetoothGatt.GATT_SUCCESS,
+                        offset,
+                        // TODO: You should send the actual data here in json
+                        "[]".toByteArray())
+                } catch (e: SecurityException) {
+                    Log.e("AdHocNet", "Failed to send response")
+                }
             }
         }
     }
@@ -81,25 +81,37 @@ class AdHocNetWorker(appContext: Context, workerParameters: WorkerParameters) :
             ) {
                 super.onConnectionStateChange(gatt, status, newState)
                 if (gatt == null || status != BluetoothGatt.GATT_SUCCESS) {
-                    // TODO: handle error
                     Log.e("AdHocNet", "Failed to connect to device ${gatt?.device}")
                     return
                 }
-
                 if (newState == BluetoothGatt.STATE_CONNECTED) {
-                    // TODO: handle the new connection
                     Log.d("AdHocNet", "Connected to device ${gatt.device}")
+                    connectedGattSet[gatt.device.address] = gatt
+                }
+                if (newState == BluetoothGatt.STATE_DISCONNECTED) {
+                    Log.d("AdHocNet", "Disconnected from device ${gatt.device}")
+                    connectedGattSet.remove(gatt.device.address)
                 }
             }
 
             override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
                 super.onServicesDiscovered(gatt, status)
                 if (gatt != null && status == BluetoothGatt.GATT_SUCCESS) {
-                    Toast.makeText(context, "Services discovered ${gatt.services}", Toast.LENGTH_LONG).show()
-                    Log.d("AdHocNet", "Services discovered ${gatt.services}")
+                    Log.d("AdHocNet", "Discovered services for device ${gatt.device}")
+                    connectedGattSet[gatt.device.address] = gatt
                 }
-                else {
-                    Log.e("AdHocNet", "Failed to discover services")
+            }
+
+            override fun onCharacteristicRead(
+                gatt: BluetoothGatt,
+                characteristic: BluetoothGattCharacteristic,
+                value: ByteArray,
+                status: Int
+            ) {
+                super.onCharacteristicRead(gatt, characteristic, value, status)
+                if (characteristic.uuid == infoCharacteristic.uuid && status == BluetoothGatt.GATT_SUCCESS) {
+                    // TODO: handle the read value
+                    Log.d("AdHocNet", "Read value: ${String(value)}")
                 }
             }
         }
@@ -109,34 +121,25 @@ class AdHocNetWorker(appContext: Context, workerParameters: WorkerParameters) :
             if (result != null) {
                 Log.d("AdHocNet", "BLE Device: $result")
                 try {
-                    result.device.connectGatt(context, true, gattCallback)
-                }
-                catch (_: SecurityException)
-                {
+                    result.device.connectGatt(context, false, gattCallback)
+                } catch (_: SecurityException) {
                     Log.e("AdHocNet", "Missing BLUETOOTH_CONNECT permission")
                 }
             }
         }
     }
-    override fun doWork(): Result
-    {
+    override fun doWork(): Result {
         val bluetoothManager : BluetoothManager = context.getSystemService(BluetoothManager::class.java)
         val bluetoothAdapter : BluetoothAdapter? = bluetoothManager.adapter
-        if (bluetoothAdapter != null)
-        {
-            if (bluetoothAdapter.isEnabled)
-            {
+        if (bluetoothAdapter != null) {
+            if (bluetoothAdapter.isEnabled) {
                 try {
                     start(bluetoothManager, bluetoothAdapter)
-                }
-                catch (e: SecurityException)
-                {
+                } catch (e: SecurityException) {
                     Log.e("AdHocNet", "Missing permission")
                     return Result.failure()
                 }
-            }
-            else
-            {
+            } else {
                 return Result.failure()
             }
 
@@ -144,19 +147,14 @@ class AdHocNetWorker(appContext: Context, workerParameters: WorkerParameters) :
                 if (bluetoothAdapter.isEnabled) {
                     Log.d("AdHocNet", "AdHocNetWorker periodic work")
                     // START PERIODIC WORK
-                    try
-                    {
-                        loop(bluetoothManager, bluetoothAdapter)
-                    }
-                    catch (e: SecurityException)
-                    {
+                    try {
+                        loop()
+                    } catch (e: SecurityException) {
                         Log.e("AdHocNet", "Missing permission")
                         return Result.failure()
                     }
                     // END PERIODIC WORK
-                }
-                else
-                {
+                } else {
                     Log.e("AdHocNet", "Bluetooth disabled")
                 }
                 runBlocking {
@@ -171,15 +169,22 @@ class AdHocNetWorker(appContext: Context, workerParameters: WorkerParameters) :
     }
 
     @RequiresPermission(allOf = [Manifest.permission.BLUETOOTH_CONNECT, Manifest.permission.BLUETOOTH_SCAN])
-    private fun start(bluetoothManager: BluetoothManager, bluetoothAdapter: BluetoothAdapter)
-    {
+    private fun start(bluetoothManager: BluetoothManager, bluetoothAdapter: BluetoothAdapter) {
         Log.d("AdHocNet", "Starting GATT server")
-        bluetoothManager.openGattServer(context,gattServerCallback)
+        this.bluetoothGattServer = bluetoothManager.openGattServer(context,gattServerCallback)
+        this.bluetoothGattServer!!.addService(infoService)
         bluetoothAdapter.bluetoothLeScanner.startScan(scanCallback)
     }
 
-    private fun loop(bluetoothManager: BluetoothManager, bluetoothAdapter: BluetoothAdapter)
-    {
-
+    private fun loop() {
+        for ((address, gatt) in connectedGattSet) {
+            gatt.getService(infoService.uuid)?.getCharacteristic(infoCharacteristic.uuid)?.let {
+                try {
+                    gatt.readCharacteristic(it)
+                } catch (e: SecurityException) {
+                    Log.e("AdHocNet", "Failed to read characteristic from $address")
+                }
+            }
+        }
     }
 }
