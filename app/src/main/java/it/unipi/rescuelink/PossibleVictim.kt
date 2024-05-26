35f8a6d55483c300a25ab79cf4fcd96090f4a226
paolo-asus <paol.palumbo@gmail.com>
2024-05-26 19:58:37 +0200

    Modificato per supportare marker semplificati "Possible Victim" e "Current Location"

--- app/src/main/java/it/unipi/rescuelink/PossibleVictim.kt
+++ app/src/main/java/it/unipi/rescuelink/PossibleVictim.kt
@@ -4,8 +4,10 @@ import com.google.android.gms.maps.model.LatLng
 
 data class PossibleVictim(
     val id: String,
-    val name: String,
+    val name: String?,
     val position: LatLng,
-    val age: Int,
-    val weight: Int,
-    val heartRate: Int)
+    val age: Int?,
+    val weight: Int?,
+    val heartRate: Int?
+)
+
