29ad5b3fda70056ff3a780bec1722824bf1c0f66
paolo-asus <paol.palumbo@gmail.com>
2024-05-26 22:29:54 +0200

    Funzione di prova

--- app/src/main/java/it/unipi/rescuelink/MainActivity.kt
+++ app/src/main/java/it/unipi/rescuelink/MainActivity.kt
@@ -5,11 +5,12 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.model.LatLng
+import trilateration.Trilateration
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,12 +23,29 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {v -> this.changeToMapsView(v)}
+        button.setOnClickListener {v -> this.testTrilateration(v)}
     }
 
     private fun changeToMapsView(v: View?)
     {
         Log.d(null, "Changing to map view")
         startActivity(Intent(this, MapsActivity::class.java))
+    }
+
+    private fun testTrilateration(v: View?){
+        val points = listOf(
+            LatLng(43.8433, 10.5031),
+            LatLng(43.8419, 10.4997),
+            LatLng(43.8421, 10.5052),
+        )
+        val distances = listOf(100.0, 200.0, 110.0)
+
+        val trilateration = Trilateration(points, distances)
+        val location = trilateration.locate()
+        Log.d(TAG, "Location: ${location.latitude}, ${location.longitude}")
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
